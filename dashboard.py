e73c6a38aefe1ecd2b7bb1068c3b3cca232def02
chaosGuppy <lawphi56@gmail.com>
2023-01-09 13:52:02 +0000

    add auc plot

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,6 @@
+import json
+
+import numpy as np
 import streamlit as st
 import pandas as pd
 import altair as alt
@@ -10,7 +13,7 @@ from difficulty import (
     quantize_difficulties,
 )
 from accuracy import get_accuracy_per_trial, get_accuracy_per_model
-from agent_characteristic import get_logistic_agent_characteristic
+from agent_characteristic import get_logistic_agent_characteristic, get_auc
 
 
 get_accuracy_per_model_with_cache = st.cache(get_accuracy_per_model)
@@ -57,6 +60,44 @@ def render_dashboard():
         create_binned_acc_plot(
             instance_accuracy_per_model, instance_difficulties, difficulty_column_name
         )
+    create_auc_plot(instance_accuracy_per_model, instance_difficulties)
+
+def create_auc_plot(
+    instance_accuracy_per_model: dict[str, list[dict[str, int]]],
+    instance_difficulties: dict[str, float],
+):
+    df = get_df_for_auc_plot(instance_accuracy_per_model, instance_difficulties)
+    chart = (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x="log(params)",
+            y="AUC",
+        )
+    )
+    st.altair_chart(chart, use_container_width=True)
+
+def get_df_for_auc_plot(
+    instance_accuracy_per_model: dict[str, list[dict[str, int]]],
+    instance_difficulties: dict[str, float],
+):
+    with open("models.json") as f:
+        params_per_model = json.load(f)
+    df_dict = {"log(params)": [], "AUC": []}
+    for model_name in st.session_state["models"]:
+        correct = {
+            result["id"]: result["is_correct"]
+            for result in instance_accuracy_per_model[model_name]
+        }
+        xs, ys = get_logistic_agent_characteristic(
+            instance_difficulties,
+            correct,
+            quantiles=st.session_state["x_axis"] == "Difficulty quantile",
+        )
+        auc = get_auc(xs, ys)
+        df_dict["log(params)"].append(np.log10(params_per_model[model_name]))
+        df_dict["AUC"].append(auc)
+    return pd.DataFrame(df_dict)
 
 
 def create_logistic_acc_plot(
